7b2fa5c15f4a6966f0b66930e8a7db56071c4e15
carah-tychewicz <10439123+carah-tychewicz@users.noreply.github.com>
2022-08-02 12:05:49 -0500

    starting to organize data

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -37,6 +37,11 @@ const CurrentWeather = () => {
           forecast ? 
           <div className="weather-forecast">
             <p>{forecast.main.temp}</p>
+            <p>{forecast.weather[0].main}</p>
+            <p>{forecast.main.feels_like}</p>
+            <p>{forecast.main.temp_min}</p>
+            <p>{forecast.main.temp_max}</p>
+            <p>{forecast.main.humidity}</p>
           </div> 
           : ''
           }
