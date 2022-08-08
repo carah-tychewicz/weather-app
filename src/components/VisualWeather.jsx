6c5f31c2fb3096ab175ec450380a4866878317da
carah-tychewicz <10439123+carah-tychewicz@users.noreply.github.com>
2022-08-07 20:25:43 -0500

    wrote a switch statement to handle what weather icons render and when.

--- src/components/VisualWeather.jsx
+++ src/components/VisualWeather.jsx
@@ -1,17 +1,58 @@
 import './VisualWeather.css';
 
 const VisualWeather = (props) => {
-  if (props) {
-    console.log(props);
-    return (
-      <div>
-        <p>Success</p>
-      </div>
-    );
-  } else {
-    return (
-      <div><p>Failure</p></div>
-    )
+  console.log(props);
+  let weatherStatus = props.location[0].main;
+
+  switch(weatherStatus) {
+    case "Clouds":
+      return (
+        <div>
+          <p>Clouds</p>
+        </div>
+      )
+    break;
+    case "Clear":
+      return (
+        <div>
+          <p>Clear</p>
+        </div>
+      )
+    break;
+    case "Snow":
+      return (
+        <div>
+          <p>Snow</p>
+        </div>
+      )
+    break;
+    case "Rain":
+      return (
+        <div>
+          <p>Rain</p>
+        </div>
+      )
+    break;
+    case "Drizzle":
+      return (
+        <div>
+          <p>Drizzle</p>
+        </div>
+      )
+    break;
+    case "Thunderstorm":
+      return (
+        <div>
+          <p>Thunderstorm</p>
+        </div>
+      )
+    break;
+    default:
+      return (
+        <div>
+          <p>Default</p>
+        </div>
+      )
   }
 }
 
